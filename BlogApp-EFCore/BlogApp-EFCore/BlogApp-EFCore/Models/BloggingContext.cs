83e8e4cec174587efee7c82e515fd396e9901dbf
Ina F. Pedersen <inapedersen90@gmail.com>
2022-10-17 12:36:21 +0200

    Added Simple blog app
    Used SQLite & Entity Framework

---
+++ BlogApp-EFCore/BlogApp-EFCore/BlogApp-EFCore/Models/BloggingContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogApp_EFCore.Models
+{
+    public class BloggingContext : DbContext
+    {
+        public DbSet<Blog> ?Blogs { get; set; }
+        public DbSet<Post> ?Posts { get; set; }
+
+        public string DbPath { get; }
+
+        public BloggingContext()
+        {
+            var folder = Environment.SpecialFolder.LocalApplicationData;
+            var path = Environment.GetFolderPath(folder);
+            DbPath = System.IO.Path.Join(path, "blogging.db");
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options) 
+            => options.UseSqlite($"Data Source={DbPath}");
+    }
+}
---
+++ BlogApp-EFCore/BlogApp-EFCore/BlogApp-EFCore/README.md
@@ -0,0 +1,11 @@
+﻿## Blog App
+Create a blog app using Entity Framework Core and SQLite
+
+## Description
+Followed along a tutorial in Microsoft Learn
+
+## Original
+You can find the tutorial here: https://learn.microsoft.com/en-us/ef/core/get-started/overview/first-app?tabs=visual-studio
+
+## Author
+Written by Ina F. Pedersen
